83b0e95a6dd56f3065ab2edd5bbf6b4254a1c830
Ansh11-1902 <Brainyanshsati@gmail.com>
2021-12-05 19:10:42 +0530

    Added location in display text

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,7 @@
-console.log("Client side javascript file is loaded!");
-
 const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
-const messageOne = document.querySelector("#message-1");
-const messageTwo = document.querySelector("#message-2");
-
-// messageOne.textContent = "From JS";
+const messageOne = document.querySelector("#msg-1");
+const messageTwo = document.querySelector("#msg-2");
 
 weatherForm.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -15,16 +11,14 @@ weatherForm.addEventListener("submit", (e) => {
   messageOne.textContent = "Loading...";
   messageTwo.textContent = "";
 
-  fetch("/weather?address=" + location).then(
-    (response) => {
-      response.json().then((data) => {
-        if (data.error) {
-          messageOne.textContent = data.error;
-        } else {
-          messageOne.textContent = data.location;
-          messageOne.textContent = data.forecast;
-        }
-      });
-    }
-  );
+  fetch("/weather?address=" + location).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        messageOne.textContent = data.error;
+      } else {
+        messageOne.textContent = data.location;
+        messageTwo.textContent = data.forecast;
+      }
+    });
+  });
 });
