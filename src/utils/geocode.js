d1bcde2910db72d97e16bc33bfd91604f21ca33e
Apzyte Gamer <brainyanshsati@gmail.com>
2021-12-20 21:01:35 +0530

    first commit

---
+++ src/utils/geocode.js
@@ -0,0 +1,24 @@
+const request = require("request");
+
+const geocode = (address, callback) => {
+  const url =
+    "https://api.mapbox.com/geocoding/v5/mapbox.places/" +
+    address +
+    ".json?access_token=pk.eyJ1IjoiYXB6eXRlZ2FtZXIiLCJhIjoiY2t3ajM5MGV3MTIzZDMxcGJ2M3V2dHphNSJ9.r5gsD0UC4nDHMPBBOK9YMg&limit=1";
+
+  request({ url: url, json: true }, (error, { body } = {}) => {
+    if (error) {
+      callback("Unable to connect to location services!", undefined);
+    } else if (body.features.length === 0) {
+      callback("Unable to find location. Try another search.", undefined);
+    } else {
+      callback(undefined, {
+        latitude: body.features[0].center[0],
+        longitude: body.features[0].center[1],
+        location: body.features[0].place_name,
+      });
+    }
+  });
+};
+
+module.exports = geocode;
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ["./templates/partials/*.hbs", "./templates/views/*.hbs"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
